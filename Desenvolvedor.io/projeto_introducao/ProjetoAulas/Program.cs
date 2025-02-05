d0961c6c4afadbb8ef8cc36825efe505d0ea9fad
Adams Daniel <dnkndanieltrab@gmail.com>
2025-02-05 11:21:37 -0300

    Adicionando arquivos

---
+++ Desenvolvedor.io/projeto_introducao/ProjetoAulas/Program.cs
@@ -0,0 +1,10 @@
+﻿Random random = new Random();
+int numeroAleatorio = random.Next(1, 101);
+Console.WriteLine(numeroAleatorio);
+
+bool req = numeroAleatorio > 50;
+Console.WriteLine(req);
+
+string status = numeroAleatorio > 50 ? "Ativo" : "Inativo";
+
+Console.WriteLine(status);
